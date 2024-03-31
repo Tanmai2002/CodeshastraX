6981ce1743bc460cae087429a70faa7cb2af0d47
Nency129 <nencypatel129@gmail.com>
2024-03-31 09:56:23 +0530

    translator

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'package:codeshastra/app/util/Translator.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
       title: "Application",
+      translations: Messages(),
+      locale: Locale('hi', 'IN'),
+      fallbackLocale: Locale('en','US'),
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
